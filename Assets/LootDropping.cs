2c8cdf28732397e02e31e7d5b32fb33f5004a4b0
H3NRILiN <so7788520@gmail.com>
2019-10-04 01:28:20 +0800

    2

--- Assets/LootDropping.cs
+++ Assets/LootDropping.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class LootDropping : MonoBehaviour
 {
+    [SerializeField] Transform m_TargetTransform;
     [SerializeField] float m_Height;
     [SerializeField] float m_RangeXMin, m_RangeXMax;
     [SerializeField] float m_RangeZMin, m_RangeZMax;
+    [SerializeField] PhysicMaterial m_PhysicMat;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +23,10 @@ public class LootDropping : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        var rangeXMin = new Vector3(m_RangeXMin, m_Height, 0) + transform.position;
-        var rangeXMax = new Vector3(m_RangeXMax, m_Height, 0) + transform.position;
-        var rangeZMin = new Vector3(0, m_Height, m_RangeZMin) + transform.position;
-        var rangeZMax = new Vector3(0, m_Height, m_RangeZMax) + transform.position;
+        var rangeXMin = new Vector3(m_RangeXMin, m_Height, 0) + m_TargetTransform.position;
+        var rangeXMax = new Vector3(m_RangeXMax, m_Height, 0) + m_TargetTransform.position;
+        var rangeZMin = new Vector3(0, m_Height, m_RangeZMin) + m_TargetTransform.position;
+        var rangeZMax = new Vector3(0, m_Height, m_RangeZMax) + m_TargetTransform.position;
 
         Gizmos.DrawLine(rangeXMin, rangeXMax);
         Gizmos.DrawLine(rangeZMin, rangeZMax);
@@ -36,11 +38,12 @@ public class LootDropping : MonoBehaviour
         spawnPoint.x = Random.Range(m_RangeXMin, m_RangeXMax);
         spawnPoint.y = m_Height;
         spawnPoint.z = Random.Range(m_RangeZMin, m_RangeZMax);
-        spawnPoint += transform.position;
-        GameObject obj = Instantiate(targetObj, spawnPoint, Random.rotation);
+        spawnPoint += m_TargetTransform.position;
+        GameObject obj = Instantiate(targetObj, spawnPoint, Random.rotation, transform);
         MeshCollider collider = obj.AddComponent<MeshCollider>();
         collider.convex = true;
-        //collider.sharedMesh = obj.GetComponent<MeshFilter>().sharedMesh;
-        obj.AddComponent<Rigidbody>();
+        collider.sharedMaterial = m_PhysicMat;
+
+        obj.AddComponent<Rigidbody>().mass = 13;
     }
 }
